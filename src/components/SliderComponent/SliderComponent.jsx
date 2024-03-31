e42133f7bf4e15dd66784e7cdb9f7117fdeedaf3
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-01 00:34:52 +0530

    Completed SliderComponent

--- src/components/SliderComponent/SliderComponent.jsx
+++ src/components/SliderComponent/SliderComponent.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./SliderComponent.module.css"; // Assuming you have a CSS module file
+import slider1Img from "../../assets/slider1.png";
+import slider2Img from "../../assets/slider2.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -7,28 +10,45 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 
 const SliderComponent = () => {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    const generatedSlides = [];
+    for (let i = 0; i < 2; i++) {
+      generatedSlides.push(
+        <SwiperSlide key={i}>
+          <img src={slider1Img} alt="demo_slider_img" />
+        </SwiperSlide>
+      );
+      generatedSlides.push(
+        <SwiperSlide key={i + 78}>
+          <img src={slider2Img} alt="demo_slider_img" />
+        </SwiperSlide>
+      );
+    }
+    setSlides(generatedSlides);
+  }, []);
+
   return (
-    <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
+    <div className={styles.sliderContainer}>
+      {" "}
+      {/* Wrap the Swiper in a container */}
+      <div className={styles.sliderWrapper}>
+        {" "}
+        {/* Apply margins to this wrapper */}
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {slides}
+        </Swiper>
+      </div>
+    </div>
   );
 };
 
