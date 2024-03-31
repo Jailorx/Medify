96ec142aaa14171e0e720a2ce85fa89b0a89d907
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-31 22:20:12 +0530

    Adding button component in hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,6 @@
 import styles from "./HeroSection.module.css";
 import heroImage from "../../assets/heroImage.png";
+import Button from "../Button/Button";
 
 const HeroSection = () => {
   return (
@@ -13,6 +14,7 @@ const HeroSection = () => {
           Connect instantly with a 24x7 specialist or choose to video visit a
           particular doctor.
         </p>
+        <Button text="Find Centers" />
       </div>
       <div className={styles.heroImage}>
         <img src={heroImage} alt="hero_Image_of_doctors" />
