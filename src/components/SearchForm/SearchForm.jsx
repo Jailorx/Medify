ef5a2493b72f8fee9653950b03148090ce39dcdb
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-31 23:11:12 +0530

    Styling stylePanel div inside searchForm Component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,11 +1,16 @@
+import Button from "../Button/Button";
 import InputField from "../InputField/InputField";
 import styles from "./SearchForm.module.css";
 
 const SearchForm = () => {
   return (
     <div className={styles.searchFormContainer}>
-      <InputField placeholder="State" />
-      <InputField placeholder="City" />
+      <div className={styles.searchPanel}>
+        <InputField placeholder="State" />
+        <InputField placeholder="City" />
+        <Button text="Search" />
+      </div>
+      <div className={styles.content}>You may be looking for</div>
     </div>
   );
 };
