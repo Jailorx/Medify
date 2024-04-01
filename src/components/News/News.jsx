449670f4fa9ef78f93218256e92b1b4030a6413a
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-01 21:51:22 +0530

    add news component

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,12 +1,23 @@
 import Card from "./Card/Card";
 import styles from "./News.module.css";
+import { useState, useEffect } from "react";
 
 const News = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const newCards = [];
+    for (let i = 0; i < 3; i++) {
+      newCards.push(<Card key={i} />);
+    }
+    setCards(newCards);
+  }, []);
+
   return (
-    <div className={styles.newContainer}>
-      <p className={styles.category}>Blog & News</p>
-      <p className={styles.heading}>Read Our Latest News</p>
-      <Card />
+    <div className={styles.newsContainer}>
+      <p className={styles.news_category}>Blog & News</p>
+      <p className={styles.news_heading}>Read Our Latest News</p>
+      <div className={styles.news_cards}>{cards}</div>
     </div>
   );
 };
