cea7be690b70334f2b5079b68ce9a866b6bb9382
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-02 17:06:11 +0530

    removing demo footer Image and adding proper content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,43 @@
 import styles from "./Footer.module.css";
-import MobileSection from "../../assets/images/mobile-section-footer.png";
+import googlePlay from "../../assets/images/google-play.png";
+import appleStore from "../../assets/images/apple-store.png";
+
 import Logo from "../Header/Navbar/Logo/Logo";
 import facebookIcon from "../../assets/icon/facebook-icon.svg";
 import twitterIcon from "../../assets/icon/twitter-icon.svg";
 import youtubeIcon from "../../assets/icon/youtube-icon.svg";
 import pinterestIcon from "../../assets/icon/pinterest-icon.svg";
+import Button from "../Button/Button";
 
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
-      <img src={MobileSection} alt="temp_mobile_section" />
+      <div className={styles.footer_app_download}>
+        <div className={styles.footer_app_download_content}>
+          <p className={styles.content_tag}>
+            Download the <span>Medify</span> App
+          </p>
+          <p className={styles.content_message}>
+            Get the link to download the app
+          </p>
+          <div className={styles.content_input}>
+            <div className={styles.content_input_number}>
+              <div className={styles.content_input_code}>+91</div>
+              <input
+                type="text"
+                placeholder="Enter your phone number"
+                maxlength="10"
+              />
+            </div>
+            <Button text="Send SMS" />
+          </div>
+          <div className={styles.content_app_store}>
+            <img src={googlePlay} alt="google_play_store_image" />
+            <img src={appleStore} alt="apple_app_store_image" />
+          </div>
+        </div>
+      </div>
+
       <div className={styles.footerWrapper}>
         <div className={styles.footer}>
           <div className={styles.footer_company}>
