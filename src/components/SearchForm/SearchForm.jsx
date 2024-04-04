bb8b3c43f19e51b3b70bc4e1f6d84e86810f57fb
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-04 22:05:53 +0530

    add searchform functionality in searchForm component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,6 +1,6 @@
 import Button from "../Button/Button";
 import Card from "../Card/Card";
-import InputField from "../InputField/InputField";
+import searchIcon from "../../assets/icon/search-icon.svg";
 import styles from "./SearchForm.module.css";
 import doctorIcon from "../../assets/icon/doctor-icon.svg";
 import ambulanceIcon from "../../assets/icon/ambulance-icon.svg";
@@ -8,14 +8,93 @@ import labsIcon from "../../assets/icon/labs-icon.svg";
 import hospitalIcon from "../../assets/icon/hospital-icon.svg";
 import medicineIcon from "../../assets/icon/medicine-icon.svg";
 
+import { useState, useEffect } from "react";
+
 const SearchForm = () => {
+  const [states, setStates] = useState([]);
+  const [selectedState, setSelectedState] = useState("");
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
+
+  useEffect(() => {
+    const getStates = async () => {
+      const responseState = await fetch(
+        `https://meddata-backend.onrender.com/states`
+      );
+      const dataState = await responseState.json();
+      setStates(dataState);
+    };
+
+    getStates();
+  }, []);
+
+  useEffect(() => {
+    const getCitiesOfState = async () => {
+      const responseCity = await fetch(
+        `https://meddata-backend.onrender.com/cities/${selectedState}`
+      );
+      const dataCity = await responseCity.json();
+      setCities(dataCity);
+    };
+
+    if (selectedState !== "") {
+      getCitiesOfState();
+    }
+  }, [selectedState]);
+
+  const handleSelectState = (e) => {
+    setSelectedState(e.target.value);
+  };
+
+  const handleSelectCity = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
   return (
     <div className={styles.searchFormContainer}>
       <div className={styles.searchPanel}>
-        <InputField placeholder="State" />
-        <InputField placeholder="City" />
+        <div className={styles.inputFieldContainer}>
+          <img src={searchIcon} alt="search_icon" />
+          {states.length > 0 ? (
+            <select onChange={handleSelectState} value={selectedState}>
+              <option value="">Select State</option>
+              {states.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <input type="text" placeholder="State" />
+          )}
+        </div>
+        <div className={styles.inputFieldContainer}>
+          <img src={searchIcon} alt="search_icon" />
+          {cities.length > 0 ? (
+            <select onChange={handleSelectCity} value={selectedCity}>
+              <option value="">Select City</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <input
+              type="text"
+              placeholder="City"
+              disabled={cities.length === 0}
+            />
+          )}
+        </div>
         <Button text="Search" />
       </div>
+
+      <div className={styles.selectedInfo}>
+        {selectedState && <p>Selected State: {selectedState}</p>}
+        {selectedCity && <p>Selected City: {selectedCity}</p>}
+      </div>
+
       <div className={styles.content}>You may be looking for</div>
       <div className={styles.cards}>
         <Card
